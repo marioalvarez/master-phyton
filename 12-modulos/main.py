133cd159365e23ee1644d3a69f47ff396d107c53
Mario Alvarez Zapata <marioalvarez.informatico@gmail.com>
2020-06-15 13:24:57 -0400

    fin de MODULOS

--- 12-modulos/main.py
+++ 12-modulos/main.py
@@ -31,6 +31,27 @@ import datetime
 print(datetime.date.today())
 
 fecha_completa = datetime.datetime.now()
-
+"""
 print(fecha_completa)
 print(fecha_completa.year)
+print(fecha_completa.month)
+print(fecha_completa.day)
+
+fecha_personalizada = fecha_completa.strftime("%d/%m/%Y, ")
+print(f"Mi fecha personalizada: {fecha_personalizada}")
+
+print(datetime.datetime.now().timestamp())
+"""
+#modulo Matematicas
+import math
+
+print("Raiz cuadrada de 10: ", math.sqrt(10))
+print("Numero pi: ", math.pi)
+#redondear al alza
+print("Redondeo del numero  : ", math.ceil(6.65789))
+#redondear a la baja
+print("Redondeo a la baja del numero  : ", math.floor(6.66881))
+
+#modulo random
+import random 
+print("numero aleatorio entre 15 y 67: ", random.randint(15,67))
