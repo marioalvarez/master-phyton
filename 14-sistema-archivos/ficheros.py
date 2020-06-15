17bb2025477f71f244c61d4872568ac6bf9ee556
Mario Alvarez Zapata <marioalvarez.informatico@gmail.com>
2020-06-15 14:09:14 -0400

    Modulo OPEN y PATHLIB

--- 14-sistema-archivos/ficheros.py
+++ 14-sistema-archivos/ficheros.py
@@ -5,9 +5,31 @@ import pathlib
 #archivo = open("14-sistema-archivos/fichero_texto.txt","a+")
 
 #abrir archivo en ruta absoluta
-ruta = str(pathlib.Path().absolute()) + "/fichero_texto.txt"
+ruta = str(pathlib.Path().absolute()) + "/14-sistema-archivos/fichero_texto.txt"
 archivo = open(ruta,"a+")
-print(ruta)
+#print(ruta)
 
 #escribir archivo
 archivo.write("******soy un texto medito desde python*****\n")
+
+#cerrar archivo
+archivo.close()
+
+# Abrir archivo
+ruta = str(pathlib.Path().absolute()) + "/14-sistema-archivos/fichero_texto.txt"
+archivo_lectura = open(ruta,"r")
+#print(ruta)
+
+#leer contenido
+#contenido = archivo_lectura.read()
+#print(contenido)
+
+#leer contenido y guardarlo en lista
+lista = archivo_lectura.readlines()
+#archivo_lectura.close()
+
+for frase in lista:
+    lista_frase = frase.split()
+    print(lista_frase)
+    #print("- " +frase.upper())#pasar frase a mayus
+    #pasar frase a lista
