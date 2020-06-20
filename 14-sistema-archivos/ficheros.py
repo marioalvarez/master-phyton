fa56a367e5cd53ff1dc5f716c6acaac9231dc0fb
Mario Alvarez Zapata <marioalvarez.informatico@gmail.com>
2020-06-20 17:12:37 -0400

    MANIIPULACION DE FICHEROS

--- 14-sistema-archivos/ficheros.py
+++ 14-sistema-archivos/ficheros.py
@@ -1,5 +1,6 @@
 from io import open
 import pathlib
+import shutil
 
 #abrir archivo
 #archivo = open("14-sistema-archivos/fichero_texto.txt","a+")
@@ -33,3 +34,33 @@ for frase in lista:
     print(lista_frase)
     #print("- " +frase.upper())#pasar frase a mayus
     #pasar frase a lista
+
+#copiar, renombrar y eliminar (Module shutil)
+ruta_original = str(pathlib.Path().absolute()) + "/14-sistema-archivos/fichero_texto.txt"
+ruta_nueva = str(pathlib.Path().absolute()) + "/14-sistema-archivos/fichero_texto2.txt"
+
+#ruta_alternativa = "../07-ejercicios/fichero_copiado.txt"
+shutil.copyfile(ruta_original, ruta_nueva)
+
+# Mover
+ruta_original = str(pathlib.Path().absolute()) + "/14-sistema-archivos/fichero_copiado.txt"
+ruta_nueva = str(pathlib.Path().absolute()) + "/14-sistema-archivos/fichero_texto2.txt"
+
+shutil.move(ruta_original,ruta_nueva)
+
+# Eliminar
+
+import os
+
+ruta_nueva = str(pathlib.Path().absolute()) + "/14-sistema-archivos/fichero_texto2.txt"
+os.remove(ruta_nueva)
+
+#Comprobar si existe
+import os.path
+
+print(os.path.abspath("../"))
+
+if os.path.isfile(os.path.abspath("./") + "Fichero_texto.txt"):
+    print("El archivo existe")
+else:
+    print("el archivo no existe")
